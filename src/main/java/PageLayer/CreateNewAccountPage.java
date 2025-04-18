751762ecc3ee9f8d6c54c23c1c66605d27cde341
KhalateNikhil <nsknikhilkhalate@gmail.com>
2025-04-18 16:14:44 +0530

    TC 6 is added by Sonali Tester

--- src/main/java/PageLayer/CreateNewAccountPage.java
+++ src/main/java/PageLayer/CreateNewAccountPage.java
@@ -65,6 +65,7 @@ public class CreateNewAccountPage extends BaseClass {
 		System.out.println("TC 3 is created by Nikhil Tester");
 		System.out.println("TC 4 is created by Sonali Tester");
 		System.out.println("TC 5 is created by Nikhil Tester");
+		System.out.println("TC 6 is created by Sonali Tester");
 	}
 	
 	
